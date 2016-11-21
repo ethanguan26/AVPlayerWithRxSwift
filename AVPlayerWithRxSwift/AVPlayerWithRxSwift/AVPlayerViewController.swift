367d393f1362f7b955a59330e8607fb8bea66a4a
guanyi <guanyi@fenrir-tec.com>
2016-11-21 20:23:35 +0800

    refresh the toolbar’s time label

--- AVPlayerWithRxSwift/AVPlayerWithRxSwift/AVPlayerViewController.swift
+++ AVPlayerWithRxSwift/AVPlayerWithRxSwift/AVPlayerViewController.swift
@@ -98,6 +98,10 @@ class AVPlayerViewController: UIViewController {
             
             let progress: Float = Float(currentTime/duration)
             self?.avToolsBar.progress.value = progress
+            
+            
+            self?.updateTimeLabel()
+            
         }
         
     }
@@ -109,6 +113,27 @@ class AVPlayerViewController: UIViewController {
         }
         return kCMTimeInvalid
     }
+    
+    func updateTimeLabel() {
+        let playerDuration: CMTime = self.playerItemDuration()
+        
+        if !playerDuration.isValid {
+            return
+        }
+        
+        let duration: Double = CMTimeGetSeconds(playerDuration)
+        let currentTime: Double = CMTimeGetSeconds(player!.currentTime())
+        
+        let elapsedTimeMin: Int = Int(currentTime/60)
+        let elapsedTimeSec: Int = Int(currentTime) - elapsedTimeMin*60
+        
+        let remainingTimeMin: Int = Int((duration - currentTime)/60)
+        let remainingTimeSec: Int = Int(duration - currentTime) - remainingTimeMin*60
+        
+//        elapsedTimeLabel.text = (NSString(format:"%02d",elapsedTimeMin) as String) + ":" + (NSString(format:"%02d",elapsedTimeSec) as String)
+//        self.remainingTimeLabel.text = "-" + (NSString(format:"%02d",remainingTimeMin) as String) + ":" + (NSString(format:"%02d",remainingTimeSec) as String)
+    }
+
     
     /// Change containers layout
     func changePlayerSize() {
